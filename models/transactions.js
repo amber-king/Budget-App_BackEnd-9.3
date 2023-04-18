46e5a0a27b5aa37f26d5e1ada82bcff2e088c2fc
amber-king <amberking@pursuit.org>
2023-04-18 09:48:41 -0400

    added ids for the object array of info

--- models/transactions.js
+++ models/transactions.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
-    
+    id: "123",
     item_name: "Income",
-    amount: 250 ,
+    amount: 250,
     date: "April 15, 2023",
     from: "Employer-Company",
     catergory: "income",
   },
   {
-   
+    id: "456",
     item_name: "Phone Bill",
     amount: 300,
     date: "April 20, 2023",
@@ -16,7 +16,7 @@ module.exports = [
     catergory: "bill",
   },
   {
-   
+    id: "789",
     item_name: "Groceries",
     amount: 500,
     date: "March 30, 2023",
@@ -24,7 +24,7 @@ module.exports = [
     catergory: "food",
   },
   {
-   
+    id: "101",
     item_name: "Vet Visit",
     amount: 900,
     date: "January 3, 2023",
